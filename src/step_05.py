09cd261453ca79e028040bb33b22f593747149b5
FlorianJacta <98709993+FlorianJacta@users.noreply.github.com>
2023-02-27 15:06:11 +0100

    Add precision on Python version for Torch

--- src/step_05.py
+++ src/step_05.py
@@ -19,6 +19,8 @@ dataframe = pd.DataFrame({"Text":[''],
                           "Score Neg":[0.33],
                           "Overall":[0]})
 
+# Torch is, for now, only available for the Python version between 3.8 and 3.10. 
+# If you cannot install these packages, just return a dictionary of random numbers for the `analyze_text(text).`
 def analyze_text(text):
     # Run for Roberta Model
     encoded_text = tokenizer(text, return_tensors='pt')
